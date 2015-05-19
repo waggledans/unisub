7f4df89fa5095cb03c8ce909d48432d51bf37fb1
Dan Slov <dan.slov@gmail.com>
2015-05-19 15:20:43 -0700

    changed DB to contain objects (as sub entries) and not tuples

--- bin/lib/parse.py
+++ bin/lib/parse.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python
-# reads and parses srt 
 
+#Copyright (c) 2015 Dan Slov
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+
+
+""" Module for parsing SubRip test files (.srt extension)  """
 # imports section
 # first add the directory to the search path
 import os
@@ -11,37 +32,81 @@ bindir = os.path.abspath(os.path.dirname(sys.argv[0]))
 sys.path.append(bindir)
 import re
 # End of imports
-class ParsedSrtFile(object):
-    subs = {}
+
+
+##### CONSTANTS ######
+class _CONST(object):
+    FOO = 1234
+    # pattern to match time of the sub line
+    # Example: 00:00:03,748 --> 00:00:06,901
+    timeFramePattern = "\t*(\d+:\d+:[\d,\,]+).*--\>.*(\d.*)"
+    # pattern to match ordinal number of the subtitle
+    currentSubNumberPattern = "^\d+$"
+    subSeparator = "^\t*$"
+    secondsInMinute = 60
+    secondsInHour = 3600
+    def __setattr__(self, *_):
+        pass
+
+CONST = _CONST()
+
+class _SrtEntry(object):
+    subNumber  = 0
+    timeFrame = ""
+    subText   = ""
+    startTime = 0
+    endTime = 1
+    def __init__(self, subNumber = 0, timeFrame = "", subText = ""):
+        self.subNumber = subNumber
+        self.timeFrame = timeFrame
+        self.subText = subText
+    def extractTimeFromTimeFrame (self):
+        if not self.timeFrame:
+            self.startTime = 0
+            self.endTime = 0
+        else:
+            match = re.match(CONST.timeFramePattern,self.timeFrame)
+            # Example: 00:00:03,748 --> 00:00:06,901
+            if (match):
+                self.startTime = self._convertSrtFormatTime(match.group(1))
+                self.endTime = self._convertSrtFormatTime(match.group(2))
+
+    def _convertSrtFormatTime (self, srtTime):
+        times = srtTime.split(':')
+        if(len(times) == 3):
+            #lastKey = match.group(1)
+            mils = float(times[2].replace(',','.'))
+            return mils + CONST.secondsInMinute*int(times[1]) + CONST.secondsInHour * int(times[0])
+        else:
+            # smth is wrong, TODO:: raise EXCEPTION
+            # or maybe just do nothing
+            return 0
+
+    def toString (self):
+        return self.subNumber + self.timeFrame + self.subText
+
+class SrtObject(object):
+    srtDB = {}
     filename = ""
-    def __init__(self, **subs):
+    def __init__(self, **srtDB):
         #  init class members ######
-        self.subs = subs
+        self.srtDB = srtDB
         self.filename = ""
 
     @classmethod
     def fromFilename(cls, filename):
         srtDB = cls()
-        srtDB.srtToHash(filename)
+        srtDB.buildSrtDB(filename)
         return srtDB
 
-    def srtToHash(self,filename = ""):
+    def buildSrtDB(self,filename = ""):
         if not filename:
             filename = self.filename
-        """ srtToHash takes .srt file name as an argument.
+        """ buildSrtDB takes .srt file name as an argument.
             The starting time of each entry is used as a dictionary key
             The value of the dictionary is the list (number, time, text)    
         """
         srtfile = open(filename,"r")
-#lines = srtfile.readlines()
-##### CONSTANTS ######
-# pattern to match time of the sub line
-        timePattern = "\t*(\d+:\d+:[\d,\,]+).*--.*\d"
-        currentNumberPattern = "^\d+$"
-        subSeparator = "^\t*$"
-        secondsInMinute = 60
-        secondsInHour = 3600
-#
 ##### temp variables ######
         saveOn = False  # saveOn is True when a line containing time to show subs is matched
         # saveOn is False when empty line is matched
@@ -51,69 +116,64 @@ class ParsedSrtFile(object):
         for line in srtfile: 
             #print(line)
             #line = line.rstrip()
-            if(re.match(currentNumberPattern, line) and not saveOn): # means we start a new sub
+            if(re.match(CONST.currentSubNumberPattern, line) and not saveOn): # means we start a new sub
                 #subNumber = int(line)
                 subNumber = line
-            match = re.match(timePattern,line)
+            match = re.match(CONST.timeFramePattern,line)
+            # Example: 00:00:03,748 --> 00:00:06,901
             if (match):
-                times = match.group(1).split(':')
-                if(len(times) == 3):
+                srtEntry = _SrtEntry(subNumber, line)
+                startTimes = match.group(1).split(':')
+                if(len(startTimes) == 3):
                     saveOn = True
                     lastKey = match.group(1)
-                    mils = float(times[2].replace(',','.'))
-                    # time variable will be used later when subtitles are not exact match
-                    time = mils + secondsInMinute*int(times[1]) + secondsInHour * int(times[0])
-                    self.subs[lastKey] = (subNumber, line, "") 
+                    self.srtDB[lastKey] = srtEntry
                 else:
                     # smth is wrong, TODO:: raise EXCEPTIONS
                     # or maybe just do nothing
                     continue
                 #print(match.group(1))
             else:
-                if (re.match(subSeparator, line)):
+                if (re.match(CONST.subSeparator, line)):
                     saveOn = False    #saveOn is false, move on to the next sub
                 if (saveOn):
                     #assuming multiline sub
-                    record = self.subs[lastKey]
-                    record = (record[0], record[1], record[2].join(line))
-                    self.subs[lastKey] = record
+                    self.srtDB[lastKey].subText = self.srtDB[lastKey].subText + line 
+        #pprint.pprint(self.srtDB)            
         srtfile.close()
-# mergeSameSrt appends subs2 to subs11
-    def mergeSameSrt(self, subs2):
+# mergeSrtDB appends subs2 to subs11
+    def mergeSrtDB(self, subs2):
         """ 
-        mergeSameSrt assumes that both dictionaries contain exactly the same set of keys
+        mergeSrtDB assumes that both dictionaries contain exactly the same set of keys
         """
         subs_merged = {}
-        for key1 in self.subs:
-            #subs[key1] = subs1[key1]
-            if (subs2.subs[key1]):
-                record1 = self.subs[key1]
-                record2 = subs2.subs[key1]
-                sub2 = record2[2]
-                record = (record1[0], record1[1], record1[2] + sub2)
+        for key1 in self.srtDB:
+            if (subs2.srtDB[key1]):
+                record1 = self.srtDB[key1]
+                record2 = subs2.srtDB[key1]
+                record = _SrtEntry (record1.subNumber, record1.timeFrame, record1.subText + record2.subText)
                 subs_merged[key1] = record
-        return ParsedSrtFile(**subs_merged)        
+        return SrtObject(**subs_merged)        
 
     def printSrt(self, filename):
         srtfile = open(filename,"w")
-        for key in sorted(self.subs):
-            for el in self.subs[key]:
-                srtfile.write(el)
+        for key in sorted(self.srtDB):
+            srtfile.write(self.srtDB[key].toString())
             srtfile.write("\n")
         srtfile.close()
 
-    def addPinyinToEnglishSrt(subs):
+    def addPinyinToEnglishSrt(self):
         """ 
         addPinyinToEnglishSrt takes dictionary and returns new dictionary 
         containing hanzi and pinyin
         """
         subs_merged = {}
-        for key1 in subs.subs:
-            record1 = subs.subs[key1]
-            pin = pinyin.get(record1[2])
-            record = (record1[0], record1[1], record1[2] + pin)
-            subs_merged[key1] = record
-        return ParsedSrtFile(subs_merged)
+        for key in self.srtDB:
+            record1 = self.srtDB[key]
+            pin = pinyin.get(record1.subText)
+            record = _SrtEntry(record1.subNumber, record1.timeFrame, record1.subText + pin)
+            subs_merged[key] = record
+        return SrtObject(**subs_merged)
 class Date(object):
     day = 0
     month = 0
--- bin/merge.py
+++ bin/merge.py
@@ -1,12 +1,31 @@
 #!/usr/bin/env python
 
-# first add the directory to the search path
+#Copyright (c) 2015 Dan Slov
+#
+#Permission is hereby granted, free of charge, to any person obtaining a copy
+#of this software and associated documentation files (the "Software"), to deal
+#in the Software without restriction, including without limitation the rights
+#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#copies of the Software, and to permit persons to whom the Software is
+#furnished to do so, subject to the following conditions:
+#
+#The above copyright notice and this permission notice shall be included in all
+#copies or substantial portions of the Software.
+#
+#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#SOFTWARE.
+
+
 import getopt
 import re
 import os
 import os.path
-import pprint
-import time
+#import time
 from lib import parse
 import sys
 reload(sys)  
@@ -19,20 +38,20 @@ def main (argv):
     file1 = sys.argv[1]
     file2 = sys.argv[2]
     if os.path.isfile(file1) and os.access(file1, os.R_OK):
-        hash1 = parse.ParsedSrtFile.fromFilename(file1)
+        srtDBToModify = parse.SrtObject.fromFilename(file1)
         if len(sys.argv) > 3 and sys.argv[3] == 'pinyin':
-            pinyin_subs = parse.addPinyinToEnglishSrt(hash1)
+            pinyin_subs = srtDBToModify.addPinyinToEnglishSrt()
             pinyin_subs.printSrt(file2)
             sys.exit(0)
     else:
         sys.exit(3)
     if os.path.isfile(file2) and os.access(file2, os.R_OK):
-        hash2 = parse.ParsedSrtFile.fromFilename(file2)
+        srtDBToAdd = parse.SrtObject.fromFilename(file2)
     else:
         sys.exit(4)
-    subs = hash1.mergeSameSrt(hash2)    
+    mergedSrtDB = srtDBToModify.mergeSrtDB(srtDBToAdd)    
     out_file = re.sub("\.srt$", "_merged.str", file1)
-    subs.printSrt(out_file)
+    mergedSrtDB.printSrt(out_file)
 
 def longopts_example(argv):
    ifile = ''
