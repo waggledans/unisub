db935d9393d956173c51aa2f22aed6ded4b8a4ce
Dan Slov <dan.slov@saucelabs.com>
2016-02-28 13:31:12 -0800

    got rid of modisub

--- tests/test_pinyin.py
+++ tests/test_pinyin.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import tempfile
 import unittest
-import os
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -10,16 +8,6 @@ from unisub import unisub
 
 
 class TestPinyin(unittest.TestCase):
-    def setUp(self):
-        super(self.__class__,self).setUp()
-        import os
-        files_dir = os.path.join(os.path.join(os.getcwd(), 'tests'), 'files')
-        self.in_file = os.path.join(files_dir, 'hanzi.srt')
-        self.ref_file = os.path.join(files_dir, 'hanzi_pinyined.srt')
-        self.srtDBToModify = unisub.SrtObject.fromFilename(self.in_file)
-        self.srtDBToAdd = self.srtDBToModify.srtHanziToPinyin()
-        self.srtDBRef = unisub.SrtObject.fromFilename(self.ref_file)
-
     def testConvertHanzi(self):
         key = "00:00:03,748 --> 00:00:06,901"
         new_sub = unisub._SrtEntry('0', key, "你好")
@@ -28,24 +16,5 @@ class TestPinyin(unittest.TestCase):
         srtConverted = srt.srtHanziToPinyin()
         self.assertEqual(srtConverted.srtDB.get(key).subText, 'nǐhǎo')
 
-    def testConvertSrtObject(self):
-        self.assertEqual(self.srtDBToAdd, self.srtDBRef)
-
-    def testSrtFileGenerate(self):
-        test_file = tempfile.NamedTemporaryFile(delete=False)
-        test_file_name = test_file.name
-        try:
-            self.srtDBToAdd.printSrt(test_file_name)
-            with open(self.ref_file, 'r') as file_ref:
-                with open(test_file_name, 'r') as file_test:
-                    diffs = list(set(file_ref) - set(file_test))
-            self.assertTrue(len(diffs) == 0)
-        finally:
-            os.remove(test_file_name)
-
     def testSrtHanziAndPinyinFileGenerate(self):
         pass
-
-    def testSrtWrongFormat(self):
-        pass
-        #assertRaises()
--- tests/test_unisub.py
+++ tests/test_unisub.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import unittest
+import tempfile
+import os
 from unisub import unisub
 
 
-class TestPinyin(unittest.TestCase):
+class TestUnisub(unittest.TestCase):
     def setUp(self):
-        super(self.__class__,self).setUp()
-        import os
+        super(self.__class__, self).setUp()
         files_dir = os.path.join(os.path.join(os.getcwd(), 'tests'), 'files')
         self.in_file = os.path.join(files_dir, 'hanzi.srt')
         self.ref_file = os.path.join(files_dir, 'hanzi_pinyined.srt')
@@ -30,3 +31,22 @@ class TestPinyin(unittest.TestCase):
         self.assertEqual("%06.3f" % new_sub.startTime, "%06.3f" % newStart)
         new_sub.shiftFrame(3.1, decr=True)
         self.assertEqual("%06.3f" % new_sub.startTime, "%06.3f" % oldStart)
+
+    def testConvertSrtObject(self):
+        self.assertEqual(self.srtDBToAdd, self.srtDBRef)
+
+    def testSrtFileGenerate(self):
+        test_file = tempfile.NamedTemporaryFile(delete=False)
+        test_file_name = test_file.name
+        try:
+            self.srtDBToAdd.printSrt(test_file_name)
+            with open(self.ref_file, 'r') as file_ref:
+                with open(test_file_name, 'r') as file_test:
+                    diffs = list(set(file_ref) - set(file_test))
+            self.assertTrue(len(diffs) == 0)
+        finally:
+            os.remove(test_file_name)
+
+    def testSrtWrongFormat(self):
+        pass
+        # assertRaises()
